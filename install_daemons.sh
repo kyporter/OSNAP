c07877cba8fc40eb3a4bf46f4ccfa14044c5f033
kyporter <kylamporter@gmail.com>
2017-01-13 10:16:23 -0800

    first attempt shell script
    with prescribed prefix instead of variable, to test if it will work(to be changed once confirmed working)

---
+++ install_daemons.sh
@@ -0,0 +1,15 @@
+curl http://www-us.apache.org/dist//httpd/httpd-2.4.25.tar.bz2 > httpd-2.4.25.tar.bz2 
+
+tar -xjf httpd-2.4.25.tar.bz2
+
+cd /home/osnapdev/httpd-2.4.25
+
+./configure --prefix=$HOME/installed
+
+make
+
+make install 
+
+cd $HOME/installed
+
+git clone https://github.com/postgres/postgres.git
