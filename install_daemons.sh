216785503d930489e8e5e209b85d2bba5dec8f30
kyporter <kylamporter@gmail.com>
2017-01-14 17:19:29 -0800

    change order, add branch checkout to get to correct version

--- install_daemons.sh
+++ install_daemons.sh
@@ -1,3 +1,16 @@
+git clone https://github.com/postgres/postgres.git
+
+cd $HOME/postgres
+
+git checkout -b REL9_5_STABLE origin/REL9_5_STABLE
+
+./configure --prefix=$HOME/installed
+
+make
+
+make install
+
+
 curl http://www-us.apache.org/dist//httpd/httpd-2.4.25.tar.bz2 > httpd-2.4.25.tar.bz2 
 
 tar -xjf httpd-2.4.25.tar.bz2
@@ -10,6 +23,4 @@ make
 
 make install 
 
-cd $HOME/installed
 
-git clone https://github.com/postgres/postgres.git
