caa1a630347415b0ff60857c77eee9301aa10a9a
Kyla Porter <kporter@uoregon.edu>
2017-01-23 07:27:34 +0000

    fixed issue with type for primary keys

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,6 +1,6 @@
 
 CREATE TABLE users(
-user_pk int serial not null, 
+user_pk serial not null, 
 username text not null,
 active boolean not null,
 PRIMARY KEY(user_pk));
@@ -14,14 +14,14 @@ user_fk int not null,
 facility_fk int not null);
 
 CREATE TABLE products(
-product_pk int serial not null,
+product_pk serial not null,
 vendor text,
 description text,
 alt_description text, 
 PRIMARY KEY(product_pk));
 
 CREATE TABLE assets(
-asset_pk int serial not null, 
+asset_pk serial not null, 
 product_fk int not null,
 asset_tag text not null,
 description text,
@@ -29,6 +29,6 @@ alt_description text,
 PRIMARY KEY(asset_pk));
 
 CREATE TABLE vehicles(
-vehicle_pk int serial not null,
+vehicle_pk serial not null,
 asset_fk int,
 PRIMARY KEY(vehicle_pk)); 
