3e1ea3d854eddf22a2e364fd9c4678042d2434c0
Kyla Porter <kporter@uoregon.edu>
2017-01-23 07:24:37 +0000

    added script to create products, assets, vehicles tables

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,3 +1,34 @@
-CREATE TABLE users(user_pk serial not null,username text not null,active boolean not null,PRIMARY KEY(user_pk));
-CREATE TABLE user_is(user_fk int not null,role_fk int not null);
-CREATE TABLE user_supports(user_fk int not null,facility_fk int not null);
+
+CREATE TABLE users(
+user_pk int serial not null, 
+username text not null,
+active boolean not null,
+PRIMARY KEY(user_pk));
+
+CREATE TABLE user_is(
+user_fk int not null,
+role_fk int not null);
+
+CREATE TABLE user_supports(
+user_fk int not null,
+facility_fk int not null);
+
+CREATE TABLE products(
+product_pk int serial not null,
+vendor text,
+description text,
+alt_description text, 
+PRIMARY KEY(product_pk));
+
+CREATE TABLE assets(
+asset_pk int serial not null, 
+product_fk int not null,
+asset_tag text not null,
+description text,
+alt_description text,
+PRIMARY KEY(asset_pk));
+
+CREATE TABLE vehicles(
+vehicle_pk int serial not null,
+asset_fk int,
+PRIMARY KEY(vehicle_pk)); 
