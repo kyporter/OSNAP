7e6dfba6e08fcc9078111bb3d33bdcc660e25beb
Kyla Porter <kporter@uoregon.edu>
2017-01-23 03:02:31 +0000

    added commands to create_tables, doesn't work

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,3 @@
-exists
+CREATE TABLE users(user_pk serial not null,username text not null,active boolean not null,PRIMARY KEY(user_pk));
+CREATE TABLE user_is(user_fk int not null,role_fk int not null);
+CREATE TABLE user_supports(user_fk int not null,facility_fk int not null);
