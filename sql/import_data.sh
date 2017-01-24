5fe6026ddff246a8d3d3db3d1f0506e1511c507d
Kyla Porter <kporter@uoregon.edu>
2017-01-24 08:44:04 +0000

    added cleanup functions to import_data

--- sql/import_data.sh
+++ sql/import_data.sh
@@ -3,3 +3,7 @@ curl https://classes.cs.uoregon.edu//17W/cis322/files/osnap_legacy.tar.gz > osna
 tar -xvzf osnap_legacy.tar.gz
 
 ./populate_tables.sh
+
+rm populate_tables.sh
+rm parseforsql.py
+rm printresults.sql
