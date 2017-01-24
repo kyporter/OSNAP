f25ded80bec4cdaa72d6f63248b2fbf33a357397
Kyla Porter <kporter@uoregon.edu>
2017-01-24 06:27:41 +0000

    parseforsql now migrates security_levels.csv and security_compartments.csv

--- sql/parseforsql.py
+++ sql/parseforsql.py
@@ -29,6 +29,30 @@ with open('./osnap_legacy/transit.csv', 'r') as f:
 		firstline = False 
 f.close()
 
+
+with open('./osnap_legacy/security_levels.csv', 'r') as f:
+	reader = csv.reader(f)
+	firstline = True
+	for row in reader:
+		if firstline:
+			next
+		else:
+			print(str.format("INSERT INTO levels (abbrv, comment) VALUES ('{}', '{}');", row[0], row[1]))
+		firstline = False
+f.close()
+
+
+with open('./osnap_legacy/security_compartments.csv', 'r') as f:
+	reader = csv.reader(f)
+	firstline = True
+	for row in reader:
+		if firstline:
+			next
+		else:
+			print(str.format("INSERT INTO compartments (abbrv, comment) VALUES ('{}', '{}');", row[0], row[1]))
+		firstline = False
+f.close()
+
 with open('./osnap_legacy/DC_inventory.csv', 'r') as f:
 	reader = csv.reader(f)
 	firstline = True
@@ -37,9 +61,10 @@ with open('./osnap_legacy/DC_inventory.csv', 'r') as f:
 			next
 		else:
 			comptag = row[3].split(":")
-			print(str.format("INSERT INTO assets (asset_tag) SELECT '{}' WHERE NOT EXISTS (SELECT asset_tag FROM assets WHERE asset_tag='{}');", row[0], row[0]))
 			print(str.format("INSERT INTO products (description) SELECT '{}' WHERE NOT EXISTS (SELECT description FROM products WHERE description='{}');",row[1],row[1]))
 			print(str.format("INSERT INTO compartments (abbrv) SELECT '{}' WHERE NOT EXISTS (SELECT abbrv FROM compartments WHERE abbrv='{}');",comptag[0],comptag[0]))
-			
+			print(str.format("INSERT INTO assets (asset_tag,product_fk) SELECT '{}', (SELECT product_pk FROM products WHERE description='{}')  WHERE NOT EXISTS (SELECT asset_tag FROM assets WHERE asset_tag='{}');", row[0], row[1], row[0]))
+			print(str.format("INSERT INTO security_tags (level_fk, compartment_fk, product_fk) VALUES ((SELECT level_pk FROM levels WHERE abbrv='{}'),(SELECT compartment_pk FROM compartments WHERE abbrv='{}'),(SELECT product_pk FROM products WHERE description='{}'));", comptag[1],comptag[0],row[1]))
+			print(str.format("INSERT INTO security_tags (level_fk, compartment_fk, asset_fk) VALUES ((SELECT level_pk FROM levels WHERE abbrv='{}'),(SELECT compartment_pk FROM compartments WHERE abbrv='{}'),(SELECT asset_pk FROM assets WHERE asset_tag='{}'));", comptag[1],comptag[0],row[0]))
 		firstline = False
 f.close()
