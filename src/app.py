581f4489ffdd79cbf14a070b571e51cdd42d8e4d
Kyla Porter <kporter@uoregon.edu>
2017-02-04 06:19:26 +0000

    pull info collected from one page to display results on another page

--- src/app.py
+++ src/app.py
@@ -37,8 +37,9 @@ def transit():
 t.unload_dt FROM facilities f1 JOIN convoys c ON f1.facility_pk = c.source_fk JOIN facilities f2 ON 
 f2.facility_pk = c.dest_fk JOIN asset_on t ON t.convoy_fk = c.convoy_pk JOIN assets a ON a.asset_pk 
 = t.asset_fk WHERE (%s) BETWEEN t.load_dt AND t.unload_dt;''', (datecheck,))
-
-        return render_template('transit_report.html', datecheck=datecheck)
+        infos = cur.fetchall()
+        facsleaving = [i[2] for i in infos]
+        return render_template('transit_report.html', datecheck=datecheck, leftfac = facsleaving)
     else:
         return render_template('report_main.html')
 
