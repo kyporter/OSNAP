1adc7779eb625466b74c8f570051c2da78833e6c
kyporter <kylamporter@gmail.com>
2017-02-06 03:40:57 -0800

    adding expunged dates

--- sql/parseforsql.py
+++ sql/parseforsql.py
@@ -128,6 +128,10 @@ with open('./osnap_legacy/NC_inventory.csv', 'r') as f:
 		if firstline:
 			next
 		else:
+			datebits = row[5].split('-')
+			if len(datebits) == 3:
+				datebits[1] = 12
+				row[5] = datebits[1] + '/' + datebits[0] + '/' + datebits[2]
 			print(str.format("INSERT INTO products (description) SELECT '{}' WHERE NOT EXISTS (SELECT description FROM products WHERE description='{}');",row[1],row[1]))
 			print(str.format("INSERT INTO assets (asset_tag) SELECT '{}'  WHERE NOT EXISTS (SELECT asset_tag FROM assets WHERE asset_tag='{}');", row[0], row[0]))
 			print(str.format("UPDATE assets SET product_fk=(SELECT product_pk FROM products WHERE description='{}') WHERE asset_tag='{}';", row[1], row[0]))
@@ -135,7 +139,8 @@ with open('./osnap_legacy/NC_inventory.csv', 'r') as f:
 				comptag = row[3].split(":")
 				print(str.format("INSERT INTO security_tags (level_fk, compartment_fk, asset_fk) VALUES ((SELECT level_pk FROM levels WHERE abbrv='{}'),(SELECT compartment_pk FROM compartments WHERE abbrv='{}'),(SELECT asset_pk FROM assets WHERE asset_tag='{}'));", comptag[1],comptag[0],row[0]))
 				print(str.format("INSERT INTO security_tags (level_fk, compartment_fk, product_fk) VALUES ((SELECT level_pk FROM levels WHERE abbrv='{}'),(SELECT compartment_pk FROM compartments WHERE abbrv='{}'),(SELECT product_pk FROM products WHERE description='{}'));", comptag[1],comptag[0],row[1]))
-		firstline = False
+			print(str.format("UPDATE asset_at SET depart_dt='{}' WHERE asset_fk=(SELECT asset_pk FROM assets WHERE asset_tag='{}');" row[5], row[0]))
+	firstline = False
 f.close()
 
 
@@ -150,7 +155,7 @@ with open('./osnap_legacy/MB005_inventory.csv', 'r') as f:
 			print(str.format("INSERT INTO assets (asset_tag) SELECT '{}' WHERE NOT EXISTS (SELECT asset_tag FROM assets WHERE asset_tag='{}');", row[0], row[0]))
 			print(str.format("UPDATE assets SET product_fk=(SELECT product_pk FROM products WHERE description='{}') WHERE asset_tag='{}';", row[1], row[0]))
 
-			print(str.format("INSERT INTO asset_at (asset_fk, facility_fk, arrive_dt) VALUES ((SELECT asset_pk FROM assets WHERE asset_tag = '{}'),(SELECT facility_pk FROM facilities WHERE common_name='MB 005'), '12/7/15');", row[0]))			
+			print(str.format("INSERT INTO asset_at (asset_fk, facility_fk, arrive_dt) VALUES ((SELECT asset_pk FROM assets WHERE asset_tag = '{}'),(SELECT facility_pk FROM facilities WHERE common_name='MB 005'), '12/7/15', '12/31/19');", row[0]))			
 		firstline = False
 f.close()
 
