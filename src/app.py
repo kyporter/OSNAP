983f5db25c1ad1eeabd74cd52f0a6876eee07066
Kyla Porter <kporter@uoregon.edu>
2017-02-06 05:44:05 +0000

    added table to display transit report results

--- src/app.py
+++ src/app.py
@@ -32,7 +32,10 @@ def report():
 @app.route('/facility_report', methods=['POST', 'GET'])
 def facility():
     if request.method == 'POST':
-        datecheck = request.form["date"]
+        day = request.form["day"]
+        month = request.form["month"]
+        year = request.form["year"]
+        datecheck = month + '/' + day + '/' + year
         facname = request.form["facility_name"]
         cur.execute('''SELECT a.asset_tag, a.description, aa.arrive_dt, aa.depart_dt 
 FROM asset_at aa JOIN facilities f ON aa.facility_fk = f.facility_pk JOIN assets a ON 
@@ -45,14 +48,16 @@ OR (((%s) >= aa.arrive_dt) AND (aa.depart_dt IS NULL)));''', (facname, datecheck
 @app.route('/transit_report', methods=['POST', 'GET'])
 def transit():
     if request.method == 'POST':
-        datecheck = request.form["date"]
+        day = request.form["day"]
+        month = request.form["month"]
+        year = request.form["year"]
+        datecheck = month + '/' + day + '/' + year
         cur.execute('''SELECT a.asset_tag, a.description, f1.common_name, t.load_dt, f2.common_name, 
 t.unload_dt FROM facilities f1 JOIN convoys c ON f1.facility_pk = c.source_fk JOIN facilities f2 ON 
 f2.facility_pk = c.dest_fk JOIN asset_on t ON t.convoy_fk = c.convoy_pk JOIN assets a ON a.asset_pk 
 = t.asset_fk WHERE (%s) BETWEEN t.load_dt AND t.unload_dt;''', (datecheck,))
         infos = cur.fetchall()
-        facsleaving = [i[2] for i in infos]
-        return render_template('transit_report.html', datecheck=datecheck, leftfac = facsleaving)
+        return render_template('transit_report.html', datecheck=datecheck, results = infos)
     else:
         return render_template('report_main.html')
 
