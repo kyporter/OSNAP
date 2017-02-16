49b0f2ce8e6f359247155b92508f0ff0c52441bd
Kyla Porter <kporter@uoregon.edu>
2017-02-16 08:31:00 +0000

    removed old files

--- sql/import_data.sh
+++
@@ -1,9 +0,0 @@
-curl https://classes.cs.uoregon.edu//17W/cis322/files/osnap_legacy.tar.gz > osnap_legacy.tar.gz
-
-tar -xvzf osnap_legacy.tar.gz
-
-python parseforsql.py > printresults.sql
-
-psql $1 -f printresults.sql
-
-rm printresults.sql
