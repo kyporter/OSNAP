44878bc47d197ee7b9d7bb5a51c2763e9dfcaffd
Kyla Porter <kporter@uoregon.edu>
2017-02-21 11:37:51 +0000

    updated data model in create_tables

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -7,4 +7,41 @@ password varchar(16) NOT NULL, /*for now, I'm using the 16 character limit from 
 plaintext password*/
 active boolean NOT NULL DEFAULT True, /*I figure eventually we'll want this value, and it's easy to 
 set up, so I put it in now*/
+role_fk integer,
 PRIMARY KEY(user_pk));
+
+CREATE TABLE roles(
+role_pk serial,
+title varchar(40) NOT NULL, --this should allow reasonable-length titles
+description text, --gives a place to specify permissions and duties
+PRIMARY KEY(role_pk));
+
+CREATE TABLE assets(
+asset_pk serial,
+asset_tag varchar(16) NOT NULL,
+description text, --I really want this to be not null but for now I'll leave it nullable
+PRIMARY KEY(asset_pk));
+
+CREATE TABLE facilities(
+facility_pk serial,
+common_name varchar(50), 
+fac_code varchar(6),
+PRIMARY KEY(facility_pk));
+
+CREATE TABLE asset_history( --still deciding if I want to have a currently at table as well or if that's too redundant
+asset_fk integer NOT NULL,
+facility_fk integer,
+arrive_dt timestamp NOT NULL,
+depart_dt timestamp);
+
+--Making for now, may delete later
+CREATE TABLE asset_at(
+asset_fk integer NOT NULL,
+facility_fk integer,
+intake timestamp NOT NULL);
+
+--Because two roles are assumed to exist:
+INSERT INTO roles (title, description) VALUES ('Logistics Officer', 'May dispose 
+of assets');
+
+INSERT INTO roles(title) VALUES ('Facilities Officer');
