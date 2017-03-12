736ddd464e078191f78bd1342fb34ab49ed78785
Kyla Porter <kporter@uoregon.edu>
2017-03-12 01:33:38 +0000

    import python works

--- import/import_data.py
+++ import/import_data.py
@@ -10,12 +10,13 @@ def read_info(inputfile, templist):
     '''
     firstline = True
     with open(inputfile, 'r') as f:
-        if firstline:
-            firstline = False
-        else:
-            for line in f:
-                line_info = line.split(',')
+        for line in f:
+            if firstline:
+                firstline = False
+            else:
+                line_info = line.strip().split(',')
                 templist.append(line_info)
+    return templist
 
 def main():
     if len(sys.argv)<3:
@@ -24,7 +25,7 @@ def main():
 
     dbname = sys.argv[1]
 
-    print("starting database connection to %s", dbname)
+    print("starting database connection to ", dbname)
 
     conn = psycopg2.connect(database=dbname, host="127.0.0.1", port="5432")
     cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor) 
@@ -37,7 +38,7 @@ def main():
     #gets user info
     userfile = dir_name + '/users.csv'
     users_info = []
-    read_info(userfile, users_info)
+    users_info = read_info(userfile, users_info)
 
     print(users_info)
 
@@ -78,7 +79,7 @@ VALUES ((%s), (%s), (SELECT role_pk FROM roles WHERE title=(%s)), (%s));''', (us
     for asset in assets_info:
         cur.execute('''INSERT INTO assets (asset_tag, description, acq_date) 
 VALUES ((%s), (%s), (%s));''', (asset[0], asset[1], asset[3]))
-	cur.execute('''INSERT INTO asset_history (asset_fk, facility_fk, 
+        cur.execute('''INSERT INTO asset_history (asset_fk, facility_fk, 
 arrive_dt) VALUES ((SELECT asset_pk FROM assets WHERE asset_tag=(%s)), (SELECT 
 facility_pk FROM facilities WHERE fac_code=(%s)), (%s));''', (asset[0], asset[2], asset[3]))
     conn.commit()
@@ -101,9 +102,7 @@ transfer[5], transfer[6], transfer[7], transfer[8]))
 asset_fk=(%s) AND facility_fk=(SELECT facility_pk FROM facilities WHERE 
 fac_code=(%s)) AND depart_dt IS NULL;''', (transfer[7], a_pk, transfer[5]))
             if transfer[8] != None:
-                cur.execute('''INSERT INTO asset_history (asset_fk, 
-facility_fk, arrive_dt) VALUES ((%s), (SELECT facility_pk FROM facilities WHERE 
-fac_code=(%s)), (%s));''', (a_pk, transfer[6], transfer[8]))
+                cur.execute('''INSERT INTO asset_history (asset_fk, facility_fk, arrive_dt) VALUES ((%s), (SELECT facility_pk FROM facilities WHERE fac_code=(%s)), (%s));''', (a_pk, transfer[6], transfer[8]))
     conn.commit()
 
 
@@ -111,12 +110,11 @@ fac_code=(%s)), (%s));''', (a_pk, transfer[6], transfer[8]))
     for asset in assets_info:
         if asset[4] != 'NULL':
             #set last known facility's departure date
-            cur.execute('''UPDATE asset_history SET depart_dt=(%s) WHERE 
-asset_fk = (SELECT asset_pk FROM assets WHERE asset_tag=(%s)) AND depart_dt IS 
-NULL AND facility_fk IS NOT NULL;''', (asset[0],))
+            cur.execute('''UPDATE asset_history SET depart_dt=(%s) WHERE asset_fk = 
+(SELECT asset_pk FROM assets WHERE asset_tag=(%s)) AND depart_dt IS NULL AND 
+facility_fk IS NOT NULL;''', (asset[4], asset[0]))
             #set disposal entry
-            cur.execute('''INSERT INTO asset_history (asset_fk, arrive_dt) 
-VALUES ((SELECT asset_pk FROM assets WHERE asset_tag=(%s)), (%s));''', (asset[0], asset[4]))
+            cur.execute('''INSERT INTO asset_history (asset_fk, arrive_dt) VALUES ((SELECT asset_pk FROM assets WHERE asset_tag=(%s)), (%s));''', (asset[0], asset[4]))
     conn.commit()
 
 if __name__=='__main__':
