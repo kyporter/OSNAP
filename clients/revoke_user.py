00dadc0f5757a5c753a91238eef04311fefa3b1c
Kyla Porter <kporter@uoregon.edu>
2017-03-19 21:26:41 +0000

    added revoke client

--- clients/revoke_user.py
+++ clients/revoke_user.py
@@ -1,18 +1,16 @@
-import
+import requests
 import sys
-import json
 
 def main():
     if len(sys.argv)<3:
         print("Usage: python3 %s <URL> <username>"%sys.argv[0])
         return
 
-    routename = sys.argv[1]
+    routename = sys.argv[1] + 'update_user'
     uname = sys.argv[2]
 
-    arguments = jsonify(update_type = "revoke", username = uname)
+    r = requests.post(routename, data ={'update_type':"revoke", 'username': uname})
     
-    return
 
 
 if __name__='__main__':
