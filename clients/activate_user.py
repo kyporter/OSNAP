2793d38c52ac278b43bfbfb81a2cf6eb13243aed
Kyla Porter <kporter@uoregon.edu>
2017-03-20 04:50:58 +0000

    updated role logic

--- clients/activate_user.py
+++ clients/activate_user.py
@@ -1,19 +1,19 @@
 import requests
 import sys
 
-role_dict = {"LO" : "Logistics Officer", "FO" : "Facilities Officer"}
+role_dict = {"logofc" : "Logistics Officer", "facofc" : "Facilities Officer"}
 
 def main():
     if len(sys.argv)<5:
-        print("Usage: python3 %s <URL> <username> <password> <role: LO or FO>"%sys.argv[0])
+        print("Usage: python3 %s <URL> <username> <password> <role: logofc or facofc>"%sys.argv[0])
         return
-    if sys.argv[4].upper() != "LO" and sys.argv[4].upper() != "FO":
+    if sys.argv[4].lower() != "logofc" and sys.argv[4].lower() != "facofc":
         print("invalid role, please enter LO or FO as role")
 
     routename = sys.argv[1] + 'update_user'
     uname = sys.argv[2]
     pword = sys.argv[3]
-    role_abb = sys.argv[4]
+    role_abb = sys.argv[4].lower()
     role = role_dict[role_abb]
 
     r = requests.post(routename, data = {'update_type':'activate', 'username':uname, 'password':pword, 'role':role})
